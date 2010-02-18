0a87b1a1211ad18e1d03ea5ceef89561e2420940
s-edwards <edwards@cs.vt.edu>
2010-02-18 07:10:02 +0000

    Patch from Godmar Back to handle errors due to miscapitalized file names (on case-insensitive OSes) correctly.

--- src/org/webcat/exceptiondoctor/handlers/NoClassDefFoundErrorHandler.java
+++ src/org/webcat/exceptiondoctor/handlers/NoClassDefFoundErrorHandler.java
@@ -1,6 +1,8 @@
 package org.webcat.exceptiondoctor.handlers;
 
 import java.io.FileNotFoundException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.webcat.exceptiondoctor.AbstractExceptionHandler;
 import org.webcat.exceptiondoctor.ExceptionHandlerInterface;
 import org.webcat.exceptiondoctor.LineNotFoundException;
@@ -17,18 +19,44 @@ public class NoClassDefFoundErrorHandler extends AbstractExceptionHandler
 		// this.exceptionName = "NoClassDefFoundError";
 	}
 
+    /*
+     * On case-insensitive Windows, the JVM may load Miscapitalized.class
+     * when looking for class 'MisCapitalized'.  In this case, a 'wrong name'
+     * is issued because the class name found in the .class file does not
+     * match the expected name.
+     */
+    private static Pattern wrongNameExcMessage
+        = Pattern.compile("(\\S+) \\(wrong name: (\\S+)\\)");
+
 	@Override
 	public Throwable wrapException(Throwable exToWrap)
 			throws FileNotFoundException, LineNotFoundException,
 			SourceCodeHiddenException
 	{
-		String newMessage =
-		    "It seems that the code was trying to use the class called "
-		    + exToWrap.getMessage()
-		    + ".  However, the Java VM could not locate the file "
-		    + exToWrap.getMessage()
-		    + ".class.  Make sure the .class file is in the right "
-		    + "directory.";
+		String newMessage = null;
+        String noCDFEMsg = exToWrap.getMessage();
+
+        Matcher m = wrongNameExcMessage.matcher(noCDFEMsg);
+        if (m.find()) {
+            String expectedClass = m.group(1);
+            String foundClass = m.group(2);
+            if (expectedClass.equalsIgnoreCase(foundClass)) {
+                newMessage =
+                      "The class " + expectedClass + " could not be found "
+                    + "because you miscapitalized the name in the .java file "
+                    + "as " + foundClass + ".";
+            }
+        }
+
+        if (newMessage == null) {
+            newMessage =
+                "It seems that the code was trying to use the class called "
+                + noCDFEMsg
+                + ".  However, the Java VM could not locate the file "
+                + noCDFEMsg
+                + ".class.  Make sure the .class file is in the right "
+                + "directory.";
+        }
 
 		return buildNewException(exToWrap, newMessage,
 				NoClassDefFoundError.class);
